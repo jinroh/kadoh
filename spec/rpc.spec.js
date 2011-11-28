df398c88868dfb7b5e3c3415f9ee2223e604fe38
Lachèze Alexandre <alexandre.lacheze@gmail.com>
2011-11-28 23:40:36 +0100

    refactor in RPC objects

--- spec/rpc.spec.js
+++ spec/rpc.spec.js
@@ -22,8 +22,8 @@ describe('RPC', function() {
     var rpc = new RPC(peer, request);
     expect(rpc.getID).toBeFunction();
     expect(rpc.getDST() instanceof Peer).toBe(true);
-    expect(rpc.getDSTSocket()).toEqual('127.0.0.1:321');
-    expect(rpc.getDSTID()).toEqual(SHA('127.0.0.1:321'));
+    expect(rpc.getDST().getSocket()).toEqual('127.0.0.1:321');
+    expect(rpc.getDST().getID()).toEqual(SHA('127.0.0.1:321'));
     expect(rpc.getRequest()).toBeObject();
   });
   
