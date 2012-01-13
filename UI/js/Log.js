d6ae02fe39abbfb7332b245fa1baaf9033dc8a65
Lachèze Alexandre <alexandre.lacheze@gmail.com>
2012-01-13 20:58:43 +0100

    Improve UI

--- UI/js/Log.js
+++ UI/js/Log.js
@@ -56,7 +56,7 @@ KadOHui.Logger =  function(console_element, control_element) {
                       message,
                     '</div>',
                     '<div class="span2">',
-                      '<time rel="twipsy" datetime="'+time.toISOString()+'" title="'+human_time+'" data-placement="right">'+human_time+'</time> ago',
+                      '<time rel="twipsy" datetime="'+time.toISOString()+'" title="'+human_time+'" data-placement="below">'+human_time+'</time> ago',
                     '</div>',
                 '</div>'].join('\n');
       return $(html);
