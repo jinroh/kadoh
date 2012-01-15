0133da5fc37f806f1d65e0a0c28313ffe7f10336
Pierre Guilleminot <pierre.guilleminot@gmail.com>
2012-01-15 14:51:04 +0100

    move CLI to /bot/bin and add the pool system

--- bots/bot.js
+++ bots/bot.js
@@ -1,10 +1,10 @@
-var Hook  = require('hook.io').Hook;
-var KadOH = require(__dirname + '/../dist/KadOH.node.js');
-var util  = require('util');
-
 //
 // Bot
 //
+
+var Hook  = require('hook.io').Hook;
+var KadOH = require(__dirname + '/../dist/KadOH.node.js');
+var util  = require('util');
 
 var Bot = exports.Bot = function(options) {
   options = options || {
@@ -40,58 +40,7 @@ Bot.prototype.k_connect = function() {
 };
 
 Bot.prototype.k_join = function() {
-  this.kadoh.join(['foo@bar.org'], function() {
-    this.emit('joined');
-  }, this);
+  this.once('bootstraps', function(bootstraps) {
+  });
+  this.emit('bootstraps');
 };
-
-//
-// CLI
-// 
-
-if (process.argv.length > 2) {
-  var argv  = require('optimist')
-              .usage('Usage: $0 --protocol xmlrpc --transport UDP -j foo@bar -r kadoh -p azerty -d 1000')
-              .alias('j', 'jid')
-              .alias('r', 'resource')
-              .alias('p', 'password')
-              .alias('d', 'delay')
-              .argv;
-
-  var debug     = !!argv.debug,
-      name      = argv.udp ? 'udpbot'   : 'xmppbot'
-      protocol  = argv.udp ? 'jsonrpc2' : 'node_xmlrpc',
-      type      = argv.udp ? 'UDP'      : 'NodeXMPP',
-      delay     = argv.delay || 0,
-      transport = {
-        jid      : argv.jid,
-        resource : argv.resource,
-        password : argv.password,
-        port     : argv.port
-      };
-  var config = {
-    hook : {
-      name: name
-    },
-    node : {
-      reactor : {
-        protocol  : protocol,
-        type      : type,
-        transport : transport
-      }
-    },
-    delay : delay
-  };
-
-  var bot = new Bot(config);
-  bot.start();
-
-  if (debug) {
-    bot.on('hook::ready', function() {
-      KadOH.log.setLevel('debug');
-      KadOH.log.subscribeTo(bot.kadoh);
-      KadOH.log.subscribeTo(bot.kadoh._reactor);
-      KadOH.log.subscribeTo(bot.kadoh._reactor._transport);
-    });
-  }
-}
--- bots/pool.js
+++ bots/pool.js
@@ -1,5 +1,76 @@
 /**
- * @TODO
  * Pool which *instanciates* a given number of nodes and 
  * spawn a new pool when full
  */
+var DEFAULT_SIZE = 30;
+var LAMBDA       = 2;
+
+var util  = require('util');
+var Hook  = require('hook.io').Hook;
+var spawn = require('child_process').spawn;
+var Bot   = require(__dirname + '/bot').Bot;
+
+var Pool = module.exports = function(size, options) {
+  Hook.call(this, {
+    name   : 'pool',
+    silent : true
+  });
+  this._options  = options;
+  this._sizeLeft = size || DEFAULT_SIZE;
+  this._nodes    = [];
+  this._launched = false;
+
+  var self = this;
+  this.on('hook::ready', function() {
+    self.launch();
+  });
+};
+util.inherits(Pool, Hook);
+
+Pool.prototype.launch = function() {
+  if (!this._launched) {
+    var n = Math.min(DEFAULT_SIZE, this._sizeLeft) - 1,
+        s = 0;
+    for (; n >= 0; n--) {
+      s += Math.floor((-Math.log(Math.random()) / LAMBDA * 1000));
+      this._nodes.push(new Bot({
+        node  : this._options,
+        hook  : {
+          name : 'bot',
+          silent : true
+        },
+        delay : s
+      }));
+      this._sizeLeft--;
+    }
+
+    this._nodes.map(function(bot) {
+      bot.start();
+    });
+
+    if (this._sizeLeft > 0) {
+      setTimeout(function(self) {
+        self._duplicate();
+      }, (DEFAULT_SIZE / LAMBDA * 1000) * (1 + 3/10), this);
+    } else {
+      this.emit('pool::dht-launched')
+    }
+    this._launched = true;
+  }
+};
+
+Pool.prototype._duplicate = function() {
+  if (this._sizeLeft > 0) {
+    var opts = this._options;
+    var args = [__filename, '--size=' + this._sizeLeft];
+    if (opts.reactor.type === 'UDP') {
+      args.push('--udp',
+                '--port=' + (opts.reactor.transport.port + 1));
+    } else {
+      args.push('--jid='  + opts.reactor.transport.jid,
+                '--resource=' + opts.reactor.transport.resource,
+                '--password=' + opts.reactor.transport.password);
+    }
+    spawn('node', args);
+  }
+};
