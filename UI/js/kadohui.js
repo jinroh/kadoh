a21e830d734a6d74f60119d18e34cbe953c03e61
Lachèze Alexandre <alexandre.lacheze@gmail.com>
2012-01-18 13:41:41 +0100

    [UI] timedif helper

--- UI/js/kadohui.js
+++ UI/js/kadohui.js
@@ -1,7 +1,32 @@
 KadOHui = (typeof KadOHui !== 'undefined') ? KadOHui : {};
 
+KadOHui.helper = {};
+
+KadOHui.helper.timeDif = function(date) {
+  var now = new Date.now();
+  date = (date instanceof Date) ? date.getTime() : date;
+  var dif = date - now;
+  var absdif = Math.abs(dif);
+
+  var D = Math.floor(absdif/(1000*60*60*24)); absdif = absdif - D*(1000*60*60*24);
+  var H = Math.floor(absdif/(1000*60*60   )); absdif = absdif - H*(1000*60*60);
+  var M = Math.floor(absdif/(1000*60      )); absdif = absdif - M*(1000*60);
+  var S = Math.floor(absdif/(1000         )); absdif = absdif - S*(1000);
+
+  return [
+    (dif>0) ? 'in'   : '',
+    (D>0)   ? D+'d ' : '',
+    (H>0)   ? H+'h ' : '',
+    (M>0)   ? M+'m ' : '',
+    (S>0)   ? S+'s ' : '',
+    (dif<0) ? 'ago'  : ''
+  ].join('');
+
+};
 
 
+KadOHui.init = function() {
+  
   var popover_options = {
       offset: 10,
       //live: true,
