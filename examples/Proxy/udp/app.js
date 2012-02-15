3eb6d1e48dfd77b65474263b7ee3342eb02a7978
Lachèze Alexandre <alexandre.lacheze@gmail.com>
2012-02-15 22:53:59 +0100

    separate UDP proxy

---
+++ examples/Proxy/udp/app.js
@@ -0,0 +1,15 @@
+var connect = require('connect'),
+    path    = require('path');
+
+
+var server = connect.createServer()
+             .use('/'      , connect.static(__dirname))
+             .use('/dist'  , connect.static(path.join(__dirname, '../../..', 'dist')))
+             .use('/jquery', connect.static(path.join(__dirname, '../../..', 'lib/ext/jquery')))
+            // .use('/benchmark', connect.static(path.join(__dirname, '../../..', 'benchmarking')))
+             .use('/UI'    , connect.static(path.join(__dirname, '../../..', 'UI')));
+
+console.log('http://localhost:8080');
+proxy = require('../../../lib/server/udpproxy').listen(server);
+
+server.listen(8080);
