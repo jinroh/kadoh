b56dcbebe4b7b779377797af944806c8e7eab297
Pierre Guilleminot <pierre.guilleminot@gmail.com>
2012-02-21 11:52:47 +0100

    cleaning the apps directory

--- apps/proxy/udp/app.js
+++ apps/proxy/udp/app.js
@@ -6,7 +6,7 @@ var server = connect.createServer()
              .use('/'      , connect.static(__dirname))
              .use('/dist'  , connect.static(path.join(__dirname, '../../..', 'dist')))
              .use('/jquery', connect.static(path.join(__dirname, '../../..', 'lib/ext/jquery')))
-            // .use('/benchmark', connect.static(path.join(__dirname, '../../..', 'benchmarking')))
+          // .use('/benchmark', connect.static(path.join(__dirname, '../../..', 'benchmarking')))
              .use('/UI'    , connect.static(path.join(__dirname, '../../..', 'UI')));
 
 console.log('http://localhost:8080');
