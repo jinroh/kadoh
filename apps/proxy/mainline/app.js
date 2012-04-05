b7ed2e568510c82f15a3fd164c9dda5af91dc858
Lachèze Alexandre <alexandre.lacheze@gmail.com>
2012-04-05 23:32:07 +0200

    cherry pick merging

--- apps/proxy/mainline/app.js
+++ apps/proxy/mainline/app.js
@@ -1,13 +1,16 @@
 var connect = require('connect'),
+    http    = require('http'),
     path    = require('path');
 
 
-var server = connect.createServer()
-             .use('/'      , connect.static(__dirname))
-             .use('/dist'  , connect.static(path.join(__dirname, '../../..', 'dist')))
-             .use('/jquery', connect.static(path.join(__dirname, '../../..', 'lib/ext/jquery')))
-            // .use('/benchmark', connect.static(path.join(__dirname, '../../..', 'benchmarking')))
-             .use('/UI'    , connect.static(path.join(__dirname, '../../..', 'UI')));
+var app = connect.createServer()
+                 .use('/'      , connect.static(__dirname))
+                 .use('/dist'  , connect.static(path.join(__dirname, '../../..', 'dist')))
+                 .use('/jquery', connect.static(path.join(__dirname, '../../..', 'lib/ext/jquery')))
+             //  .use('/benchmark', connect.static(path.join(__dirname, '../../..', 'benchmarking')))
+                 .use('/UI'    , connect.static(path.join(__dirname, '../../..', 'UI')));
+
+var server = exports.server = http.createServer(app);
 
 console.log('http://localhost:8080');
 proxy = require('../../../lib/server/mainlineproxy').listen(server);
--- apps/proxy/udp/app.js
+++ apps/proxy/udp/app.js
@@ -1,15 +1,17 @@
 var connect = require('connect'),
+    http    = require('http'),
     path    = require('path');
 
 
-var server = connect.createServer()
-             .use('/'      , connect.static(__dirname))
-             .use('/dist'  , connect.static(path.join(__dirname, '../../..', 'dist')))
-             .use('/jquery', connect.static(path.join(__dirname, '../../..', 'lib/ext/jquery')))
-          // .use('/benchmark', connect.static(path.join(__dirname, '../../..', 'benchmarking')))
-             .use('/UI'    , connect.static(path.join(__dirname, '../../..', 'UI')));
+var app = connect.createServer()
+                 .use('/'      , connect.static(__dirname))
+                 .use('/dist'  , connect.static(path.join(__dirname, '../../..', 'dist')))
+                 .use('/jquery', connect.static(path.join(__dirname, '../../..', 'lib/ext/jquery')))
+             //  .use('/benchmark', connect.static(path.join(__dirname, '../../..', 'benchmarking')))
+                 .use('/UI'    , connect.static(path.join(__dirname, '../../..', 'UI')));
 
-console.log('http://localhost:8080');
+var server = exports.server = http.createServer(app);
+
 proxy = require('../../../lib/server/udpproxy').listen(server);
 
 server.listen(8080);
