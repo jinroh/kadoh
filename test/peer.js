78c273c3f66d3290a79a404f92f180c9fec51c5c
Lachèze Alexandre <alexandre.lacheze@gmail.com>
2012-05-14 00:34:01 +0200

    first complete test

--- test/peer.js
+++ test/peer.js
@@ -1,77 +1,66 @@
-var should = require('chai').should();
+require('chai').should();
+var expect = require('chai').expect;
 
 describe('Peer', function() {
-  var Peer;
+  var Peer,
+   ip = '234.5.78.4:1234',
+   id = 'adb3bf3007abe686cf742851fe561bd1890b6295';
   
   beforeEach(function() {
-    Peer = require('../lib/dht/peer')
+    Peer = require('../lib/dht/peer');
   });
 
   it('should be there', function() {
     Peer.should.be.ok;
-  });
-  /*
-  var rID = function() {
-    return KadOH.util.Crypto.digest.randomSHA1();
-  }
-
-  beforeEach(function() {
-    Peer = KadOH.Peer;
-    SHA1 = KadOH.globals.DIGEST;
-    ip = '234.5.78.4:1234';
-    id = rID();
-  });
-  
-  it('should be a function', function() {
-    expect(Peer).toBeFunction();
+    Peer.should.be.a('function');
   });
   
   describe('When I instantiate a new Peer', function() {
-    
+    var peer;
+
     beforeEach(function() {
       peer = new Peer(ip, id);
     });
     
     it('should respond to `getAddress()` and `getID()`', function() {
-      expect(typeof peer.getAddress).toBe('function');
-      expect(typeof peer.getID).toBe('function');
+      peer.getAddress.should.be.a('function');
+      peer.getID.should.be.a('function');
     });
 
     it('should get a socket which is the ip:port string', function() {
-      expect(peer.getAddress()).toEqual(ip);
+      expect(peer.getAddress()).to.equal(ip);
     });
 
     it('should get the ID', function() {
-      expect(peer.getID()).toEqual(id);
+      expect(peer.getID()).to.equal(id);
     });
 
   });
   
   it('should be possible to instanciate a peer using a triple', function() {
-    var peer1 = new Peer(['127.0.0.1:1234', SHA1('foo')]);
-    var peer2 = new Peer('127.0.0.1:1234', SHA1('foo'));
-    expect(peer1.getAddress()).toEqual('127.0.0.1:1234');
-    expect(peer1.getID()).toEqual(SHA1('foo'));
-    expect(peer1.equals(peer2)).toBeTruthy();
+    var peer1 = new Peer(['127.0.0.1:1234', id]);
+    var peer2 = new Peer('127.0.0.1:1234', id);
+    expect(peer1.getAddress()).to.equal('127.0.0.1:1234');
+    expect(peer1.getID()).to.equal(id);
+    expect(peer1.equals(peer2)).to.be.true;
   });
 
   it('should be possible to test their equality', function() {
-    var id1 = rID();
-    var id2 = rID();
+    var id1 = 'adb3bf3007abe686cf742851fe561bd1890b6295';
+    var id2 = '0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33';
     var peer1 = new Peer(['127.0.0.1:1234', id1]);
     var peer2 = new Peer('127.0.0.1:1234', id1);
     var peer3 = new Peer(['127.0.0.1:1235', id2]);
     
-    expect(peer1.equals(peer2)).toBeTruthy();
-    expect(peer1.equals(peer3)).toBeFalsy();
+    expect(peer1.equals(peer2)).to.be.true;
+    expect(peer1.equals(peer3)).to.be.false;
   });
 
   it('should prevent bad ID', function() {
     var test = function() {
       var peer = new Peer(ip, 'abc');
     };
-    expect(test).toThrow();
+    expect(test).to.throw(Error);
   });
-*/
   
 });
