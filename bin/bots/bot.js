e8d993cee72909a550b20a276b38355426302cf5
Pierre Guilleminot <pierre.guilleminot@gmail.com>
2012-05-17 15:26:54 +0200

    Improve `dht` CLI
    The --config flag is not required anymore and neither is the extension
    of the file.

--- bin/bots/bot.js
+++ bin/bots/bot.js
@@ -5,7 +5,7 @@ var Node = require(__dirname + '/../../lib/node');
 var SHA1 = require(__dirname + '/../../lib/util/crypto').digest.SHA1;
 var Reporter = require(__dirname + '/../../lib/ext/cube/reporter');
 var logging = require(__dirname + '/../../lib/logging');
-var ConsoleLogger = require(__dirname + '/  ../../lib/logger/reporter/color-console')
+var ConsoleLogger = require(__dirname + '/../../lib/logger/reporter/color-console')
 
 new ConsoleLogger(logging, 'error');
 
